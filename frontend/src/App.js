e11e59eaeb87c3c164fd008dda0becdd17ddad5d
Teodora55 <beljicteodora55@gmail.com>
2023-08-19 16:45:29 +0200

    dodata home stranica

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React, { useState } from "react";
 import Login from "./pages/page/LoginPage/Login";
 import RegistrationPage from "./pages/page/RegistrationPage/RegistrationPage";
+import Home from "./pages/page/Home";
+import User from "./pages/page/UserPage/User";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/user" element={<User />} />
         <Route path="/register" element={<RegistrationPage />} />
         <Route path="/login" element={<Login />} />
       </Routes>
